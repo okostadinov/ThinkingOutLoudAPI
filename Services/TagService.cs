ca0437cf70ed5c0baba9c4a131e02a2c15c2a595
Oleg Kostadinov <o.o.kostadinov@gmail.com>
2024-03-09 12:49:36 +0200

    add tags controller, service; filter blogs by tag

---
+++ Services/TagService.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using ThinkingOutLoud.Data;
+using ThinkingOutLoud.Models;
+
+namespace ThinkingOutLoud.Services;
+
+public class TagService
+{
+
+    private readonly ThinkingOutLoudContext _context;
+
+    public TagService(ThinkingOutLoudContext context)
+    {
+        _context = context;
+    }
+
+    public Task<List<Tag>> GetAll()
+    {
+        return _context.Tags.ToListAsync();
+    }
+
+    public Task<Tag?> GetById(int id)
+    {
+        return _context.Tags
+            .AsNoTracking()
+            .SingleOrDefaultAsync(b => b.Id == id);
+    }
+
+    public async Task<Tag> Create(Tag tag)
+    {
+        _context.Tags.Add(tag);
+        await _context.SaveChangesAsync();
+        return tag;
+    }
+
+    public async Task Delete(int id)
+    {
+        var tag = await _context.Tags.FindAsync(id);
+
+        if (tag is not null)
+        {
+            _context.Tags.Remove(tag);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
